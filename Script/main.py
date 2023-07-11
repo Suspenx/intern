35fc8baa01ccbf94feeb1331dc3d87b9b58b7ca4
Suspenx <vaughndalespence@gmail.com>
2023-07-11 10:37:30 -0500

    Update main.py

--- Script/main.py
+++ Script/main.py
@@ -4,9 +4,12 @@ import pandas as pd
 import csv
 import re
 import numpy as np
+from scipy.io import wavfile
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 from library.Data import ignore
 get_files = getsav()
-
+from sklearn.neighbors import KNeighborsClassifier
 # function returns names, telephone number and address of respondents 
 def filter_(data_frame=frame()): 
     filter=[]
@@ -74,3 +77,22 @@ def filter_Number(data_frame):
             if isinstance(numb, int) and len(numb)<=10 :
                 number.append(numb)
 
+
+
+# Create the classifier
+classifier = MLPClassifier(hidden_layer_sizes=(100, 100), activation='relu', solver='adam',
+                     learning_rate='adaptive', max_iter=1000)
+# Load the data
+data = np.loadtxt()
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(data[:, :-1], data[:, -1], test_size=0.2)
+# Train the model
+classifier.fit(X_train, y_train)
+# Make predictions on the test set
+y_pred = classifier.predict(X_test)
+# Calculate the accuracy
+accuracy = np.mean(y_pred == y_test)
+print('Accuracy:', accuracy)
+# Get the name from the file
+name = classifier.predict(np.loadtxt())
+print('Name:', name)
